312454b2dfd9285df7bef521c6b140f5a377aa5f
BhumitaFarkunde <159683380+BhumitaFarkunde@users.noreply.github.com>
2024-09-24 10:24:42 +0000

    variable

---
+++ 01_Basic/1_variable.js
@@ -0,0 +1,11 @@
+const accounId=2345;
+let accountName="bhumita";
+var accountCity="nagpur";
+let accountAdd;
+
+ 
+ accountName="bali";
+ accountCity="amgaon";
+ accountAdd;
+
+console.table([accounId,accountName,accountCity,accountAdd]);
