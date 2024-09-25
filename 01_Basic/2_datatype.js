e6f5f25740c88e21a7e99e233223010f4e4eaf7a
BhumitaFarkunde <159683380+BhumitaFarkunde@users.noreply.github.com>
2024-09-25 06:49:40 +0000

    Datatype

---
+++ 01_Basic/2_datatype.js
@@ -0,0 +1,27 @@
+//alert()--we are using node js ,not browser
+
+/*
+number
+bigint
+string
+boolean
+null=>standalone value/blank
+undefine
+symbol=>unique
+*/
+//object
+
+let name="bhumita";
+let age=24;
+let isloggin=false;
+let city;
+
+console.log(typeof false);
+console.log(typeof age);
+console.log(typeof city);
+console.log(typeof name);
+console.log(typeof null)//object
+
+
+
+
