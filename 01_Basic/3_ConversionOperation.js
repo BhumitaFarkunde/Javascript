7f854d3f902432266207a3042225f6f3225f26eb
BhumitaFarkunde <159683380+BhumitaFarkunde@users.noreply.github.com>
2024-09-26 11:17:20 +0000

    ConversionOperation

---
+++ 01_Basic/3_ConversionOperation.js
@@ -0,0 +1,20 @@
+let score=33;
+console.log(typeof score);//number
+
+let Score="33ab";
+console.log(typeof Score);//string
+ 
+let ScoreInNumber=Number(Score);
+console.log(typeof ScoreInNumber);//number
+console.log(ScoreInNumber)
+
+ 
+let on=1;
+let booleanOn = Boolean(on);
+console.log(booleanOn);
+
+
+let someNumber=33;
+let StringNumber= String(someNumber);
+console.log(StringNumber);
+console.log(typeof StringNumber);
