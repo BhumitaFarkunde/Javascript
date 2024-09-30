1430c55962bffd4198dd8e7a663f4412245e5c2e
BhumitaFarkunde <159683380+BhumitaFarkunde@users.noreply.github.com>
2024-09-30 08:48:11 +0000

    Concat

--- 01_Basic/3_ConversionOperation.js
+++ 01_Basic/3_ConversionOperation.js
@@ -18,3 +18,19 @@ let someNumber=33;
 let StringNumber= String(someNumber);
 console.log(StringNumber);
 console.log(typeof StringNumber);
+
+
+console.log(2+2);    //4  
+console.log("1"+2);  //12 phle string h to bad wale string me hi count honge
+console.log(1+"2");  //12 1 as it tthen 2string h to concat hua
+console.log("1"+2+2);//122 phle string h to  strin g main hi count hua
+console.log(1+2+"2");//32 phle number h to unka sum hua 3 and the 2 as it is likha gya h
+
+
+
+
+
+
+
+
+
