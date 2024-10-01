4082da084b8868cba206ad5afdbac9bb50433d1c
BhumitaFarkunde <159683380+BhumitaFarkunde@users.noreply.github.com>
2024-10-01 06:06:01 +0000

    Comparision

---
+++ 01_Basic/4_Comparision.js
@@ -0,0 +1,20 @@
+console.log("2">1);
+console.log(1>"2");
+console.log(3<"2");
+console.log("02">1);
+
+
+
+
+console.log(null>0); //in both three null case answer will be vary
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+
+// == there is no check of detatype....if the datatype of compaire elementis fiffrent then they convert internalyy their datatype and makr it same
+// === there is check datatype when we compaire two element 
+
+
